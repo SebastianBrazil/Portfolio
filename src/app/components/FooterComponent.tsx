935bd3e2a63607e7538f4d4f5417fc3352a89ad7
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-18 16:00:18 -0700

    expanded skills

--- src/app/components/FooterComponent.tsx
+++ src/app/components/FooterComponent.tsx
@@ -25,8 +25,8 @@ const FooterComponent = () => {
                 <img className='w-16 h-16 mx-6 cursor-pointer' onClick={() => goToResume()} src={resumeImg.src} alt="Resume Link" />
             </div>
             <div className='md:flex max-md:grid justify-center my-4'>
-                <p className='text-[#9e7653] max-md:text-center md:text-lg quando md:px-10' >sbrevesanchez088@gmail.com</p>
-                <p className='text-[#9e7653] max-md:text-center md:text-lg quando md:px-2' >(209) 662-9916 -- Texts preferred</p>
+                <p className='text-[#9e7653] max-md:text-center md:text-lg quando md:px-4' >sebastianbsanchez67@gmail.com</p>
+                <p className='text-[#9e7653] max-md:text-center md:text-lg quando md:px-4' >(209) 227-0161</p>
             </div>
             <div className='flex justify-center'>
                 <p className='text-[#9e7653] text-sm quando pt-4'>Made in Next.js</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,22 @@ import html from "./assets/skillsImages/html.png"
 import js from "./assets/skillsImages/js.png"
 import next from "./assets/skillsImages/next.png"
 import react from "./assets/skillsImages/react.png"
-import sql from "./assets/skillsImages/sql.png"
+// import sql from "./assets/skillsImages/sql.png"
 import ts from "./assets/skillsImages/ts.png"
 import tw from "./assets/skillsImages/tw.png"
 import unity from "./assets/skillsImages/unity.png"
 import vercel from "./assets/skillsImages/vercel.png"
 import vsc from "./assets/skillsImages/vsc.png"
+import ang from "./assets/skillsImages/ang.png"
+import devops from "./assets/skillsImages/devops.png"
+import jira from "./assets/skillsImages/jira.png"
+import notion from "./assets/skillsImages/notion.png"
+import php from "./assets/skillsImages/php.png"
+import post from "./assets/skillsImages/postman.png"
+import slack from "./assets/skillsImages/slack.png"
+import sqlite from "./assets/skillsImages/sqlite.png"
+import ssqqll from "./assets/skillsImages/ssqqll.png"
+import python from "./assets/skillsImages/python.png"
 import me from "./assets/croppedMe.jpeg"
 
 export default function Home() {
@@ -35,7 +45,7 @@ export default function Home() {
             <p className='text-[#9e7653] my-4 text-4xl text-center quando'>SKILLS</p>
 
             <p className="text-[#9e7653] mb-4 text-2xl text-center quando">Languages, Libraries, and Frameworks</p>
-            <div className='md:flex justify-evenly w-screen my-2 max-md:grid max-md:grid-cols-2'>
+            <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
                   <img className='w-16 h-16 mx-6 p-1' src={html.src} alt="HTML" />
@@ -74,7 +84,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className='md:flex justify-evenly w-screen my-2 max-md:grid max-md:grid-cols-2'>
+            <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
                   <img className='w-16 h-16 mx-6 py-1' src={bs.src} alt="BootStrap" />
@@ -99,15 +109,43 @@ export default function Home() {
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Next.js</p>
               </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={ang.src} alt="Angular" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Angular</p>
+              </div>
+            </div>
+
+            <p className="text-[#9e7653] mt-6 text-xl text-center quando">Practicing</p>
+            <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={php.src} alt="PHP" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">PHP</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={python.src} alt="Python" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Python</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={ssqqll.src} alt="SQL" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">SQL</p>
+              </div>
             </div>
 
             <p className="text-[#9e7653] my-4 text-2xl text-center quando">Programs and Tools</p>
-            <div className='md:flex justify-evenly w-screen my-2 max-md:grid max-md:grid-cols-2'>
+            <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={sql.src} alt="MySql" />
+                  <img className='w-16 h-16 mx-6' src={sqlite.src} alt="SQLite" />
                 </div>
-                <p className="text-center quando text-base text-[#9e7653]">MySQL</p>
+                <p className="text-center quando text-base text-[#9e7653]">SQLite</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
@@ -140,55 +178,90 @@ export default function Home() {
                 <p className="text-center quando text-base text-[#9e7653]">VS Code</p>
               </div>
             </div>
+            
+            <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={post.src} alt="Postman" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Postman</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={slack.src} alt="Slack" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Slack</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6 p-1' src={notion.src} alt="Notion" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Notion</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={jira.src} alt="Jira" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Jira</p>
+              </div>
+              <div className="max-md:mt-3">
+                <div className="max-md:flex max-md:justify-center">
+                  <img className='w-16 h-16 mx-6' src={devops.src} alt="Azure DevOps" />
+                </div>
+                <p className="text-center quando text-base text-[#9e7653]">Azure DevOps</p>
+              </div>
+            </div>
 
           </div>
 
           <div className='grid justify-center pt-5'>
             <p className='text-[#9e7653] my-4 text-4xl text-center quando'>NOTABLE PROJECTS</p>
 
-            <div className="md:flex md:justify-evenly w-screen max-md:grid max-md:justify-center">
-              <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
-                <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">MockTalks</p>
-                <img src={mockTalkImg.src} className="pb-3" alt="MockTalks Front Page" />
-                <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://mocktalks.vercel.app">MockTalks Website</a></span></p>
-                <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/ItzTeeler/mocktalks">MockTalks Frontend</a></span></p>
-                <p className="quando text-base mt-3 text-[#9e7653]">This website allows anyone to practice their interviewing skills. Users sign in, write a bit about themselves, and then schedule an appointment where they are paired with an available peer. Created in the Next.js framework, this was a group project. I worked mostly on the backend of the project, using C# to write an API and host it to Azure. However, I did do some work on the frontend, most notably the profile section and the associated modals to create and update it.</p>
+            <div className="grid w-screen justify-center">
+              <div className="md:flex md:justify-evenly w-screen max-w-[1920px] max-md:grid max-md:justify-center">
+                <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
+                  <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">MockTalks</p>
+                  <img src={mockTalkImg.src} className="pb-3" alt="MockTalks Front Page" />
+                  <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://mocktalks.vercel.app">MockTalks Website</a></span></p>
+                  <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/ItzTeeler/mocktalks">MockTalks Frontend</a></span></p>
+                  <p className="quando text-base mt-3 text-[#9e7653]">This website allows anyone to practice their interviewing skills. Users sign in, write a bit about themselves, and then schedule an appointment where they are paired with an available peer. Created in the Next.js framework, this was a group project. I worked mostly on the backend of the project, using C# to write an API and host it to Azure. However, I did do some work on the frontend, most notably the profile section and the associated modals to create and update it.</p>
+                </div>
+
+                <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
+                  <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Pokemon Index</p>
+                  <img src={pokeImg.src} className="pb-3" alt="Pokemon Index Front Page" />
+                  <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://pokemon-react-rebuild-index.vercel.app">Pokemon React Site</a></span></p>
+                  <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/Pokemon-React-Rebuild">Pokemon-React-Rebuild</a></span></p>
+                  <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes the React library and TailwindCSS to create an application that calls on the Poke API and allows the user to search for any Pokémon before the fifth gneration, displaying some relevant data about said Pokémon. The user can also save Pokémon, which allows them quicker access to that Pokémon. This app was originally built in vanilla JS, but was rebuilt in React.</p>
+                </div>
               </div>
 
-              <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
-                <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Pokemon Index</p>
-                <img src={pokeImg.src} className="pb-3" alt="Pokemon Index Front Page" />
-                <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://pokemon-react-rebuild-index.vercel.app">Pokemon React Site</a></span></p>
-                <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/Pokemon-React-Rebuild">Pokemon-React-Rebuild</a></span></p>
-                <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes the React library and TailwindCSS to create an application that calls on the Poke API and allows the user to search for any Pokémon before the fifth gneration, displaying some relevant data about said Pokémon. The user can also save Pokémon, which allows them quicker access to that Pokémon. This app was originally built in vanilla JS, but was rebuilt in React.</p>
-              </div>
-            </div>
+              <div className="md:flex md:justify-evenly max-w-[1920px] w-screen max-md:grid max-md:justify-center">
+                <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
+                  <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Rock Paper Scissors Lizard Spock</p>
+                  <img src={rpsls.src} className="pb-3" alt="Pokemon Index Front Page" />
+                  <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://rpsls-six.vercel.app">RPSLS Site</a></span></p>
+                  <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/RPSLS">RPSLS Front & Backend</a></span></p>
+                  <p className="quando text-base mt-3 text-[#9e7653]">{"This website is a slightly more complex game of rock paper scissors, created in vanilla JavaScript with a built-in API. The user can choose to play as one player or two players, and the single player option calls on a third-party API to simulate an opponent’s response."}</p>
+                </div>
 
-            <div className="md:flex md:justify-evenly w-screen max-md:grid max-md:justify-center">
-              <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
-                <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Rock Paper Scissors Lizard Spock</p>
-                <img src={rpsls.src} className="pb-3" alt="Pokemon Index Front Page" />
-                <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://rpsls-six.vercel.app">RPSLS Site</a></span></p>
-                <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/RPSLS">RPSLS Front & Backend</a></span></p>
-                <p className="quando text-base mt-3 text-[#9e7653]">{"This website is a slightly more complex game of rock paper scissors, created in vanilla JavaScript with a built-in API. The user can choose to play as one player or two players, and the single player option calls on a third-party API to simulate an opponent’s response."}</p>
-              </div>
-
-              <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
-                <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">&quot;All For One&quot; Minigame Website</p>
-                <img src={aforr.src} className="pb-3" alt="MockTalks Front Page" />
-                <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://aforr.vercel.appp">All For One Website</a></span></p>
-                <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/AFORR">All For One Frontend</a></span></p>
-                <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes React to display ten minigames that are intended to show off the use of a custom API that I created in C#. The website calls on the API which is hosted via Microsoft Azure.</p>
+                <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
+                  <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">&quot;All For One&quot; Minigame Website</p>
+                  <img src={aforr.src} className="pb-3" alt="MockTalks Front Page" />
+                  <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://aforr.vercel.appp">All For One Website</a></span></p>
+                  <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/AFORR">All For One Frontend</a></span></p>
+                  <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes React to display ten minigames that are intended to show off the use of a custom API that I created in C#. The website calls on the API which is hosted via Microsoft Azure.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='mb-14'>
+        <div className='mb-14 grid justify-center w-screen'>
           <p className='text-[#9e7653] my-4 text-4xl text-center quando pb-10'>ABOUT ME</p>
-          <div className='md:flex max-md:grid justify-center max-lg:px-10'>
-            <p className='quando text-2xl text-[#9e7653] md:w-[40%] md:mr-20'>I, Sebastian Breve-Sanchez, am a web developer. I currently attend CodeStack Academy, where I was first taught how to code. From here I learned all of my current programming skillset, which ranges from Unity to fullstack web development. I have experience with frontend languages such as Javascript and libraries such as React, as well as knowing C# and how to create and host an API.</p>
-            <div className='max-md:flex pt-6 max-md:pt-10 max-md:justify-center'>
+          <div className='lg:flex max-lg:grid max-w-[1920px] justify-center max-lg:px-10'>
+            <p className='quando text-2xl text-[#9e7653] lg:w-[40%] lg:mr-20'>I, Sebastian Breve-Sanchez, am a web developer. I am attending CodeStack Academy, where I was first taught how to code. From here I've had 1100+ hours to learn my current programming skillset, which ranges from Unity to fullstack web development. I have experience with frontend languages such as Javascript and libraries such as React, as well as knowing C# and how to create and host an API. Currently I am going through an internship through CodeStack, and I'm practicing PHP, Python, and SQL when I can.</p>
+            <div className='max-lg:flex pt-6 max-lg:pt-10 max-lg:justify-center'>
               <img className='w-56 h-56 lg:w-80 lg:h-80 rounded-full' src={me.src} alt="A picture of myself" />
             </div>
           </div>
