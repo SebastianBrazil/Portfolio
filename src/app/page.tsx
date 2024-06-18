e96cae212a514459a91fd706e828e81ab1a4397c
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-18 16:15:01 -0700

    fixed apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image";
 import FooterComponent from "./components/FooterComponent";
 import NavBarComponent from "./components/NavBarComponent";
 import pokeImg from "./assets/pokeImg.png"
@@ -48,37 +49,37 @@ export default function Home() {
             <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 p-1' src={html.src} alt="HTML" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 p-1' src={html.src} alt="HTML" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">HTML</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 p-1' src={css.src} alt="CSS" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 p-1' src={css.src} alt="CSS" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">CSS</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 rounded-md' src={js.src} alt="JavaScript" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 rounded-md' src={js.src} alt="JavaScript" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">JavaScript</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={ts.src} alt="TypeScript" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={ts.src} alt="TypeScript" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">TypeScript</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={cSharp.src} alt="C Sharp" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={cSharp.src} alt="C Sharp" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">C#</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={dotnet.src} alt="Dotnet" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={dotnet.src} alt="Dotnet" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">.Net</p>
               </div>
@@ -87,31 +88,31 @@ export default function Home() {
             <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 py-1' src={bs.src} alt="BootStrap" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 py-1' src={bs.src} alt="BootStrap" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">BootStrap</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 py-2' src={tw.src} alt="TailwindCSS" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 py-2' src={tw.src} alt="TailwindCSS" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">TailwindCSS</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 py-1' src={react.src} alt="React" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 py-1' src={react.src} alt="React" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">React</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={next.src} alt="Next.js" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={next.src} alt="Next.js" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Next.js</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={ang.src} alt="Angular" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={ang.src} alt="Angular" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Angular</p>
               </div>
@@ -121,19 +122,19 @@ export default function Home() {
             <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={php.src} alt="PHP" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={php.src} alt="PHP" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">PHP</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={python.src} alt="Python" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={python.src} alt="Python" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Python</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={ssqqll.src} alt="SQL" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={ssqqll.src} alt="SQL" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">SQL</p>
               </div>
@@ -143,37 +144,37 @@ export default function Home() {
             <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={sqlite.src} alt="SQLite" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={sqlite.src} alt="SQLite" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">SQLite</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={vercel.src} alt="Vercel" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={vercel.src} alt="Vercel" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Vercel</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 p-1' src={azure.src} alt="Azure" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 p-1' src={azure.src} alt="Azure" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Azure</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={github.src} alt="Github" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={github.src} alt="Github" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Github</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={unity.src} alt="Unity" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={unity.src} alt="Unity" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Unity</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={vsc.src} alt="Visual Studio Code" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={vsc.src} alt="Visual Studio Code" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">VS Code</p>
               </div>
@@ -182,31 +183,31 @@ export default function Home() {
             <div className='md:flex justify-evenly w-screen max-w-[1920px] my-2 max-md:grid max-md:grid-cols-2'>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={post.src} alt="Postman" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={post.src} alt="Postman" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Postman</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={slack.src} alt="Slack" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={slack.src} alt="Slack" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Slack</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6 p-1' src={notion.src} alt="Notion" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6 p-1' src={notion.src} alt="Notion" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Notion</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={jira.src} alt="Jira" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={jira.src} alt="Jira" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Jira</p>
               </div>
               <div className="max-md:mt-3">
                 <div className="max-md:flex max-md:justify-center">
-                  <img className='w-16 h-16 mx-6' src={devops.src} alt="Azure DevOps" />
+                  <Image width={64} height={64} className='w-16 h-16 mx-6' src={devops.src} alt="Azure DevOps" />
                 </div>
                 <p className="text-center quando text-base text-[#9e7653]">Azure DevOps</p>
               </div>
@@ -221,7 +222,7 @@ export default function Home() {
               <div className="md:flex md:justify-evenly w-screen max-w-[1920px] max-md:grid max-md:justify-center">
                 <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
                   <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">MockTalks</p>
-                  <img src={mockTalkImg.src} className="pb-3" alt="MockTalks Front Page" />
+                  <Image width={500} height={500} src={mockTalkImg.src} className="pb-3" alt="MockTalks Front Page" />
                   <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://mocktalks.vercel.app">MockTalks Website</a></span></p>
                   <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/ItzTeeler/mocktalks">MockTalks Frontend</a></span></p>
                   <p className="quando text-base mt-3 text-[#9e7653]">This website allows anyone to practice their interviewing skills. Users sign in, write a bit about themselves, and then schedule an appointment where they are paired with an available peer. Created in the Next.js framework, this was a group project. I worked mostly on the backend of the project, using C# to write an API and host it to Azure. However, I did do some work on the frontend, most notably the profile section and the associated modals to create and update it.</p>
@@ -229,7 +230,7 @@ export default function Home() {
 
                 <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
                   <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Pokemon Index</p>
-                  <img src={pokeImg.src} className="pb-3" alt="Pokemon Index Front Page" />
+                  <Image width={500} height={500} src={pokeImg.src} className="pb-3" alt="Pokemon Index Front Page" />
                   <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://pokemon-react-rebuild-index.vercel.app">Pokemon React Site</a></span></p>
                   <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/Pokemon-React-Rebuild">Pokemon-React-Rebuild</a></span></p>
                   <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes the React library and TailwindCSS to create an application that calls on the Poke API and allows the user to search for any Pokémon before the fifth gneration, displaying some relevant data about said Pokémon. The user can also save Pokémon, which allows them quicker access to that Pokémon. This app was originally built in vanilla JS, but was rebuilt in React.</p>
@@ -239,7 +240,7 @@ export default function Home() {
               <div className="md:flex md:justify-evenly max-w-[1920px] w-screen max-md:grid max-md:justify-center">
                 <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
                   <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">Rock Paper Scissors Lizard Spock</p>
-                  <img src={rpsls.src} className="pb-3" alt="Pokemon Index Front Page" />
+                  <Image width={500} height={500} src={rpsls.src} className="pb-3" alt="Pokemon Index Front Page" />
                   <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://rpsls-six.vercel.app">RPSLS Site</a></span></p>
                   <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/RPSLS">RPSLS Front & Backend</a></span></p>
                   <p className="quando text-base mt-3 text-[#9e7653]">{"This website is a slightly more complex game of rock paper scissors, created in vanilla JavaScript with a built-in API. The user can choose to play as one player or two players, and the single player option calls on a third-party API to simulate an opponent’s response."}</p>
@@ -247,7 +248,7 @@ export default function Home() {
 
                 <div className='bg-[#FFFEF7] my-2 p-3 w-80 lg:w-[500px]'>
                   <p className="text-[40px] sedan text-[#9e7653] text-center mb-3">&quot;All For One&quot; Minigame Website</p>
-                  <img src={aforr.src} className="pb-3" alt="MockTalks Front Page" />
+                  <Image width={500} height={500} src={aforr.src} className="pb-3" alt="MockTalks Front Page" />
                   <p className="quando text-base text-[#9e7653]">Vercel: <span><a className="underline" target="_blank" href="https://aforr.vercel.appp">All For One Website</a></span></p>
                   <p className="quando text-base text-[#9e7653]">Github: <span><a className="underline" target="_blank" href="https://github.com/SebastianBrazil/AFORR">All For One Frontend</a></span></p>
                   <p className="quando text-base mt-3 text-[#9e7653]">This website utilizes React to display ten minigames that are intended to show off the use of a custom API that I created in C#. The website calls on the API which is hosted via Microsoft Azure.</p>
@@ -260,9 +261,9 @@ export default function Home() {
         <div className='mb-14 grid justify-center w-screen'>
           <p className='text-[#9e7653] my-4 text-4xl text-center quando pb-10'>ABOUT ME</p>
           <div className='lg:flex max-lg:grid max-w-[1920px] justify-center max-lg:px-10'>
-            <p className='quando text-2xl text-[#9e7653] lg:w-[40%] lg:mr-20'>I, Sebastian Breve-Sanchez, am a web developer. I am attending CodeStack Academy, where I was first taught how to code. From here I've had 1100+ hours to learn my current programming skillset, which ranges from Unity to fullstack web development. I have experience with frontend languages such as Javascript and libraries such as React, as well as knowing C# and how to create and host an API. Currently I am going through an internship through CodeStack, and I'm practicing PHP, Python, and SQL when I can.</p>
+            <p className='quando text-2xl text-[#9e7653] lg:w-[40%] lg:mr-20'>I, Sebastian Breve-Sanchez, am a web developer. I am attending CodeStack Academy, where I was first taught how to code. From here I&apos;ve had 1100+ hours to learn my current programming skillset, which ranges from Unity to fullstack web development. I have experience with frontend languages such as Javascript and libraries such as React, as well as knowing C# and how to create and host an API. Currently I am going through an internship through CodeStack, and I&apos;m practicing PHP, Python, and SQL when I can.</p>
             <div className='max-lg:flex pt-6 max-lg:pt-10 max-lg:justify-center'>
-              <img className='w-56 h-56 lg:w-80 lg:h-80 rounded-full' src={me.src} alt="A picture of myself" />
+              <Image width={400} height={400} className='w-56 h-56 lg:w-80 lg:h-80 rounded-full' src={me.src} alt="A picture of myself" />
             </div>
           </div>
         </div>
