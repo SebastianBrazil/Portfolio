f760369d5bc83962ce5e776a8d3e6fb99853e7d0
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-20 11:28:46 -0700

    nothing happened in 1989

--- src/app/components/Google.tsx
+++ src/app/components/Google.tsx
@@ -5,14 +5,14 @@ import Script from "next/script";
 const Google = () => {
     return (
         <>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id={process.env.GOOGLE_ID}" />
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3BZVZDGD73" />
             <Script id="googleA" strategy="afterInteractive">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    gtag('config', '{process.env.GOOGLE_ID}');
+                    gtag('config', 'G-3BZVZDGD73');
                 `}
             </Script>
         </>
